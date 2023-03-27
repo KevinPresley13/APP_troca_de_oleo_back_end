a801f8ec81fe9113dbe4fbf73df69c3af4b106a1
Kevin Presley <kevinpresley1995@gmail.com>
2023-03-27 20:21:00 -0300

    criando a tabela Troca

--- src/models/Tocas.ts
+++ src/models/Tocas.ts
@@ -10,3 +10,36 @@ export interface OleoInstance extends Model{
     data_troca: string,
     proxima_troca:string
 };
+
+export const Troca = sequelize.define<OleoInstance>('Troca',{
+    id:{
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+        autoIncrement: true
+    },
+    nome_cliente: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    telefone_cliente: {
+        type: DataTypes.STRING,
+    },
+    nome_oleo: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    odometro: {
+        type: DataTypes.INTEGER
+    },
+    data_troca: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    proxima_troca: {
+        type: DataTypes.DATE,
+        allowNull: false
+    }
+},{
+    tableName: 'troca_de_oleo',
+    timestamps: false
+});
