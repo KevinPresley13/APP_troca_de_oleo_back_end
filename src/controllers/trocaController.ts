7a5f6eec846e67ffc55ea2aa2f76dd01872ba8f1
Kevin Presley <kevinpresley1995@gmail.com>
2023-03-28 20:03:26 -0300

    pegando todos arquivos do banco

--- src/controllers/trocaController.ts
+++ src/controllers/trocaController.ts
@@ -1,10 +1,7 @@
 import { Request, Response } from 'express';
+import { Troca } from '../models/Trocas';
 import { sequelize } from "../instances/mysql";
-export const home = async (req: Request, res: Response) => {
-    try{
-        await sequelize.authenticate()
-        console.log('parabens')
-    }catch{
-        console.log('erro')
-    }
+export const getAll = async (req: Request, res: Response) => {
+    let trocaList = await Troca.findAll();
+    res.json({trocaList})
 }
--- src/routers/index.ts
+++ src/routers/index.ts
@@ -2,6 +2,6 @@ import { Router } from "express";
 import * as trocaController from "../controllers/trocaController";
 const router = Router();
 
-router.get('/troca', trocaController.home);
+router.get('/trocas', trocaController.getAll);
 
 export default router;
