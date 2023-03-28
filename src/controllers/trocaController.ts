57b4cfe0eac0b71f026c5431945b6759fab42011
Kevin Presley <kevinpresley1995@gmail.com>
2023-03-28 20:28:06 -0300

    criando rota de add nova troca

--- src/controllers/trocaController.ts
+++ src/controllers/trocaController.ts
@@ -1,7 +1,24 @@
 import { Request, Response } from 'express';
 import { Troca } from '../models/Trocas';
 import { sequelize } from "../instances/mysql";
+
+
 export const getAll = async (req: Request, res: Response) => {
     let trocaList = await Troca.findAll();
     res.json({trocaList})
-}
+};
+
+export const add =async (req: Request, res: Response) => {
+    if(req.body.nome_cliente){
+        let newTroca = await Troca.create({
+            nome_cliente: req.body.nome_cliente,
+            telefone_cliente: req.body.telefone_cliente,
+            nome_oleo: req.body.nome_oleo,
+            odometro: req.body.odometro,
+            data_troca: req.body.data_troca,
+            proxima_troca: req.body.proxima_troca
+        })
+    }else{
+        res.json({error: 'Erro ao criar nova troca!'})
+    };
+};
--- src/routers/index.ts
+++ src/routers/index.ts
@@ -3,5 +3,6 @@ import * as trocaController from "../controllers/trocaController";
 const router = Router();
 
 router.get('/trocas', trocaController.getAll);
+router.post('/troca', trocaController.add);
 
 export default router;
