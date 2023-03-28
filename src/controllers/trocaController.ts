7baf1b801b78d0cbdcb77c9a5dd83c5f184e889e
Kevin Presley <kevinpresley1995@gmail.com>
2023-03-28 20:38:08 -0300

    rota de deletar troca

--- src/controllers/trocaController.ts
+++ src/controllers/trocaController.ts
@@ -18,7 +18,20 @@ export const add =async (req: Request, res: Response) => {
             data_troca: req.body.data_troca,
             proxima_troca: req.body.proxima_troca
         })
+        res.json({ newTroca });
     }else{
         res.json({error: 'Erro ao criar nova troca!'})
     };
 };
+export const remove = async (req: Request, res: Response) => {
+    const id: string = req.params.id;
+    let troca = await Troca.findByPk(id);
+    if(troca){
+        await troca.destroy();
+        res.json({ msg: 'troca excluida com sucesso!'})
+    }else{
+        res.json({ msg: 'erro na exclusão!'})
+    }
+};
+
+
--- src/routers/index.ts
+++ src/routers/index.ts
@@ -4,5 +4,6 @@ const router = Router();
 
 router.get('/trocas', trocaController.getAll);
 router.post('/troca', trocaController.add);
+router.delete('/troca/:id', trocaController.remove);
 
 export default router;
